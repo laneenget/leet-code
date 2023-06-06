b291a3f2949cdb6de6e5e3e2a82293fa9b5d2025
Lane Enget <enget006@umn.edu>
2023-06-06 14:12:17 -0500

    Solve the two sum problem using a HashMap

--- two-sum/TwoSum.java
+++ two-sum/TwoSum.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
+
 public class TwoSum {
 
+    // Solution in O(n^2) time
     public int[] twoSum(int[] nums, int target) {
         
         boolean foundSolution = false;
@@ -23,8 +26,30 @@ public class TwoSum {
         return solutionList;
     }
 
-    public int[] twoSumHash(int[] nums, int target) {
-        int[] solutionList = new int[2];
-        return solutionList;
+    // Solution in O(n) time
+    // HashMap is useful to store the key-value of pairs of the num array
+    // so we can look up the number that allows us to hit the target
+    public int[] twoSumHashMap(int[] nums, int target) {
+        HashMap<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+        indexMap = createIndexMap(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer requiredNum = (Integer)(target - nums[i]);
+            if (indexMap.containsKey(requiredNum)){
+                int solutionList[] = {indexMap.get(requiredNum), i};
+                return solutionList;
+            }
+        }
+        return null;
+    }
+
+    public HashMap<Integer, Integer> createIndexMap(int[] nums) {
+        HashMap<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            indexMap.put(nums[i], i);
+        }
+        
+        return indexMap;
     }
 }
